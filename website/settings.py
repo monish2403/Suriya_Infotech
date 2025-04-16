d786c55b099253cfe93345ac39a95c549401d40a
k. monish raaj <k.monishraaj24@gmail.com>
2025-04-16 20:35:25 +0530

    Login Page Added

--- website/settings.py
+++ website/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'suriya_infotech',
         'USER': 'postgres',
-        'PASSWORD': 'root',
+        'PASSWORD': 'monish',
         'HOST': 'localhost',
         'PORT': '5432',
         
