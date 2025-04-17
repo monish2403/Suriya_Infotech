6e0948893d943b743d593460292ac4979de9f14f
monish2403 <k.monishraaj24@gmail.com>
2025-04-17 20:37:51 +0530

    sorting cards

--- suriya_infotech/views.py
+++ suriya_infotech/views.py
@@ -7,11 +7,11 @@ def home(request):
     return render(request,'home.html')
 
 def view_laptop(request):
-    laptops = Laptop.objects.all() 
+    laptops = Laptop.objects.all().order_by('price')
     return render(request,'laptop.html',{'laptops': laptops})
 
 def view_desktop(request):
-    desktops = Desktop.objects.all() 
+    desktops = Desktop.objects.all().order_by('price')
     return render(request,'desktop.html',{'desktops': desktops})
 
 def view_refurbished(request):
@@ -24,9 +24,9 @@ def view_gaming(request):
     return render(request, 'gaming.html')
 
 def view_refurbished_laptop(request):
-    laptops = Refurbished_Laptop.objects.all()
+    laptops = Refurbished_Laptop.objects.all().order_by('price')
     return render(request, 'laptop.html',{'laptops': laptops})
 
 def view_refurbished_desktop(request):
-    desktops = Refurbished_Desktop.objects.all()
+    desktops = Refurbished_Desktop.objects.all().order_by('price')
     return render(request, 'desktop.html',{'desktops': desktops})
